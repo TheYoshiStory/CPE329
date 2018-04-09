759ef446dfd1a5c0a3d2349b67765be9b45a357d
Ayusman <AyusmanSaha@gmail.com>
2018-04-09 09:29:35 -0700

    fixed us delay constant

--- A2/delay.h
+++ A2/delay.h
@@ -3,7 +3,7 @@
 
 #define CLK_FREQ 3000000
 #define DELAY_MS CLK_FREQ / 1000
-#define DELAY_US CLK_FREQ / 1000000
+#define DELAY_US CLK_FREQ / 1700000
 
 void delay_ms(int ms, int clk);
 void delay_us(int us, int clk);
