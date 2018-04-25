f65a064b2df2a749f7651f23f814f9c1effa21a9
Ayusman <Ayusman@207.62.154.103>
2018-04-25 10:57:42 -0700

    2-bit counter generator for A5 in P2

--- P2/main.c
+++ P2/main.c
@@ -1,33 +1,37 @@
 #include "msp.h"
 
 #define TIMER_CTRL P6
-#define CLK_FREQ 24000000
+#define CLK_FREQ 1500000
+#define SIGNAL_FREQ 1000
 #define DUTY_CYCLE 50
-#define SIGNAL_FREQ 25000
 
 // main program
 void main()
 {
+    /*
     // enable MCLK on P4.3
     P4->SEL1 &= ~BIT3;
     P4->SEL0 |= BIT3;
     P4->DIR |= BIT3;
+    */
 
-    // configure lower 2 bits as GPIO output
+    // configure lower 2 bits in TIMER_CTRL as GPIO output
     TIMER_CTRL->SEL1 &= ~(BIT1|BIT0);
     TIMER_CTRL->SEL0 &= ~(BIT1|BIT0);
     TIMER_CTRL->DIR |= BIT1|BIT0;
     TIMER_CTRL->OUT &= ~(BIT1|BIT0);
 
-    // set DCO to 24MHz and use as MCLK and SMCLK
+    // set DCO to 1.5MHz and use as MCLK and SMCLK
     CS->KEY = CS_KEY_VAL;
     CS->CTL0 = 0;
-    CS->CTL0 = CS_CTL0_DCORSEL_4;
+    CS->CTL0 = CS_CTL0_DCORSEL_0;
     CS->CTL1 = CS_CTL1_SELA_2 | CS_CTL1_SELS_3 | CS_CTL1_SELM_3;
     CS->KEY = 0;
 
     // initialize timer A0
+    TIMER_A0->CCTL[1] = TIMER_A_CCTLN_CCIE;
     TIMER_A0->CCTL[0] = TIMER_A_CCTLN_CCIE;
+    TIMER_A0->CCR[1] = (2 * CLK_FREQ * DUTY_CYCLE) / (SIGNAL_FREQ * 100);
     TIMER_A0->CCR[0] = (CLK_FREQ * DUTY_CYCLE) / (SIGNAL_FREQ * 100);
     TIMER_A0->CTL = TIMER_A_CTL_SSEL__SMCLK | TIMER_A_CTL_MC__CONTINUOUS;
     SCB->SCR |= SCB_SCR_SLEEPONEXIT_Msk;
@@ -41,23 +45,38 @@ void main()
     }
 }
 
-
 // timer A0 interrupt service routine
 void TA0_0_IRQHandler()
 {
-    TIMER_CTRL->OUT |= BIT1;
-    TIMER_A0->CCTL[0] &= ~TIMER_A_CCTLN_CCIFG;
-
-    if(TIMER_CTRL->OUT & BIT0)
+    if(TIMER_A0->CCTL[1] & TIMER_A_CCTLN_CCIFG)
     {
-        TIMER_CTRL->OUT &= ~BIT0;
-        TIMER_A0->CCR[0] += (CLK_FREQ * (100 - DUTY_CYCLE)) / (SIGNAL_FREQ * 100);
-    }
-    else
-    {
-        TIMER_CTRL->OUT |= BIT0;
-        TIMER_A0->CCR[0] += (CLK_FREQ * DUTY_CYCLE) / (SIGNAL_FREQ * 100);
+        TIMER_A0->CCTL[1] &= ~TIMER_A_CCTLN_CCIFG;
+
+        if(TIMER_CTRL->OUT & BIT1)
+        {
+            TIMER_A0->CCR[1] += (2 * CLK_FREQ * (100 - DUTY_CYCLE)) / (SIGNAL_FREQ * 100);
+            TIMER_CTRL->OUT &= ~BIT1;
+        }
+        else
+        {
+            TIMER_A0->CCR[1] += (2 * CLK_FREQ * DUTY_CYCLE) / (SIGNAL_FREQ * 100);
+            TIMER_CTRL->OUT |= BIT1;
+        }
     }
 
-    TIMER_CTRL->OUT &= ~BIT1;
+    if(TIMER_A0->CCTL[0] & TIMER_A_CCTLN_CCIFG)
+    {
+        TIMER_A0->CCTL[0] &= ~TIMER_A_CCTLN_CCIFG;
+
+        if(TIMER_CTRL->OUT & BIT0)
+        {
+            TIMER_A0->CCR[0] += (CLK_FREQ * (100 - DUTY_CYCLE)) / (SIGNAL_FREQ * 100);
+            TIMER_CTRL->OUT &= ~BIT0;
+        }
+        else
+        {
+            TIMER_A0->CCR[0] += (CLK_FREQ * DUTY_CYCLE) / (SIGNAL_FREQ * 100);
+            TIMER_CTRL->OUT |= BIT0;
+        }
+    }
 }
