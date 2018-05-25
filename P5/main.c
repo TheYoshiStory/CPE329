5d14c2f0b9057678d4c9c02b245170cb862903b7
Ayusman <Ayusman@129.65.233.29>
2018-05-25 11:56:53 -0700

    Working I2C code

--- P5/main.c
+++ P5/main.c
@@ -7,6 +7,56 @@
 #include "esc.h"
 
 volatile channel ch[6];
+uint16_t TransmitFlag = 0;
+
+void write_mpu(uint8_t reg, uint8_t data)
+{
+  EUSCI_B0->CTLW0 |= EUSCI_B_CTLW0_TR;          // Set transmit mode (write)
+  EUSCI_B0->CTLW0 |= EUSCI_B_CTLW0_TXSTT;       // I2C start condition
+
+  while (!TransmitFlag);                // Wait for EEPROM address to transmit
+  TransmitFlag = 0;
+
+  EUSCI_B0 -> TXBUF = reg;    // Send the high byte of the memory address
+
+  while (!TransmitFlag);            // Wait for the transmit to complete
+  TransmitFlag = 0;
+
+  EUSCI_B0 -> TXBUF = data;    // Send the high byte of the memory address
+
+  while (!TransmitFlag);            // Wait for the transmit to complete
+  TransmitFlag = 0;
+
+  EUSCI_B0 -> CTLW0 |= EUSCI_B_CTLW0_TXSTP;   // I2C stop condition
+}
+
+uint8_t read_mpu(uint8_t reg)
+{
+  EUSCI_B0->CTLW0 |= EUSCI_B_CTLW0_TR;      // Set transmit mode (write)
+  EUSCI_B0->CTLW0 |= EUSCI_B_CTLW0_TXSTT;   // I2C start condition
+
+  while (!TransmitFlag);                // Wait for EEPROM address to transmit
+  TransmitFlag = 0;
+
+  EUSCI_B0 -> TXBUF = reg;    // Send the high byte of the memory address
+
+  while (!TransmitFlag);            // Wait for the transmit to complete
+  TransmitFlag = 0;
+
+  EUSCI_B0->CTLW0 &= ~EUSCI_B_CTLW0_TR;   // Set receive mode (read)
+  EUSCI_B0->CTLW0 |= EUSCI_B_CTLW0_TXSTT; // I2C start condition (restart)
+
+  // Wait for start to be transmitted
+  while ((EUSCI_B0->CTLW0 & EUSCI_B_CTLW0_TXSTT));
+
+  // set stop bit to trigger after first byte
+  EUSCI_B0->CTLW0 |= EUSCI_B_CTLW0_TXSTP;
+
+  while (!TransmitFlag);            // Wait to receive a byte
+  TransmitFlag = 0;
+
+  return(EUSCI_B0->RXBUF);
+}
 
 // system initialization
 void init()
@@ -18,7 +68,7 @@ void init()
     init_dco();
     init_led();
     init_battery();
-    init_imu();
+    //init_imu();
     init_rc(ch);
     init_esc();
 
@@ -30,7 +80,32 @@ void init()
 void main()
 {
     init();
+
+    P1->SEL0 |= BIT6 | BIT7;                // Set I2C pins of eUSCI_B0
+
+    // Enable eUSCIB0 interrupt in NVIC module
+    NVIC->ISER[0] = 1 << ((EUSCIB0_IRQn) & 31);
+
+    // Configure USCI_B0 for I2C mode
+    EUSCI_B0->CTLW0 |= EUSCI_A_CTLW0_SWRST;   // Software reset enabled
+    EUSCI_B0->CTLW0 = EUSCI_A_CTLW0_SWRST |   // Remain eUSCI in reset mode
+            EUSCI_B_CTLW0_MODE_3 |            // I2C mode
+            EUSCI_B_CTLW0_MST |               // Master mode
+            EUSCI_B_CTLW0_SYNC |              // Sync mode
+            EUSCI_B_CTLW0_SSEL__SMCLK;        // SMCLK
+
+    EUSCI_B0->BRW = CLK_FREQ / IMU_FREQ;
+    EUSCI_B0->I2CSA = IMU_ADDR;          // Slave address
+    EUSCI_B0->CTLW0 &= ~EUSCI_A_CTLW0_SWRST;  // Release eUSCI from reset
+
+    EUSCI_B0->IE |= EUSCI_A_IE_RXIE |         // Enable receive interrupt
+                    EUSCI_A_IE_TXIE;
+
     blue_led();
+
+    write_mpu(0x6B,0x01);
+    delay_ms(5);
+    P2->OUT |= read_mpu(0x6B) & 0x07;
 
     while(1)
     {
@@ -38,6 +113,21 @@ void main()
         TIMER_A0->CCR[2] = ch[2].pulse;
         TIMER_A0->CCR[3] = ch[2].pulse;
         TIMER_A0->CCR[4] = ch[2].pulse;
+    }
+}
+
+void EUSCIB0_IRQHandler(void)
+{
+    if (EUSCI_B0->IFG & EUSCI_B_IFG_TXIFG0)     // Check if transmit complete
+    {
+        EUSCI_B0->IFG &= ~ EUSCI_B_IFG_TXIFG0;  // Clear interrupt flag
+        TransmitFlag = 1;                       // Set global flag
+    }
+
+    if (EUSCI_B0->IFG & EUSCI_B_IFG_RXIFG0)     // Check if receive complete
+    {
+        EUSCI_B0->IFG &= ~ EUSCI_B_IFG_RXIFG0;  // Clear interrupt flag
+        TransmitFlag = 1;                       // Set global flag
     }
 }
 
